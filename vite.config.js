75fdc39cdb25f4e8ae8a099da33cf5a43c1c5fec
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-04-08 14:00:50 +0900

    ebalrot

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
     port: 80, // you can replace this port with any port
+    https: true,
   },
   build: {
     rollupOptions: {
