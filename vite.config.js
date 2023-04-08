88b8d32a0188c4adbaa130cc9af9b74a837eae40
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-04-08 10:05:55 +0900

    wow

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     rollupOptions: {
       maxParallelFileOps: 2,
       output: {
+        cache: false,
         sourcemap: true,
         manualChunks: (id) => {
           if (id.includes("node_modules")) {
